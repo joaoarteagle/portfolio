92b24b5212717f007e02d5b0efae90d48f8ed1e2
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2024-04-09 19:26:01 -0300

    Adicionando Js ao projeto criando um Sticky menu e melhorando responsividade

--- build/index.js
+++ build/index.js
@@ -1,1 +1,13 @@
 "use strict";
+window.onscroll = function () { myFunction(); };
+const navbar = document.querySelector("#NavBar");
+const sticky = navbar.offsetTop;
+function myFunction() {
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky");
+    }
+    else {
+        navbar.classList.remove("sticky");
+    }
+}
+;
