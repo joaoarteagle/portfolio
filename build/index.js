64c27546c76d77aebfbb2fffcad63897874c3061
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2024-04-11 17:25:19 -0300

    melhorando responsividade da página

--- build/index.js
+++ build/index.js
@@ -1,8 +1,19 @@
 "use strict";
-window.onscroll = function () { myFunction(); };
+window.onscroll = function () { stickyMenu(); };
 const navbar = document.querySelector("#NavBar");
 const sticky = navbar.offsetTop;
-function myFunction() {
+const skillText = document.querySelector(".SoftSkillsContent");
+const skillPosition = skillText.offsetTop;
+function SkillsContent() {
+    if (window.screenY >= skillPosition) {
+        skillText.classList.add("scroll");
+    }
+    else {
+        skillText.classList.remove("scroll");
+    }
+}
+function stickyMenu() {
+    console.log(sticky);
     if (window.scrollY >= sticky) {
         navbar.classList.add("sticky");
     }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,16 +1,41 @@
 
 // When the user scrolls the page, execute myFunction
- window.onscroll = function() {myFunction()};
-
+ window.onscroll = function() {stickyMenu()};
  
 // Get the navbar
 const navbar = document.querySelector("#NavBar") as HTMLElement;
-
 // Get the offset position of the navbar
 const sticky = navbar.offsetTop;
 
+
+
+//Get the SoftSkills Container
+const skillText = document.querySelector(".SoftSkillsContent") as HTMLElement;
+const skillPosition = skillText.offsetTop;
+
+
+
+
+function SkillsContent(){
+
+
+        if(window.screenY >= skillPosition ){
+            skillText.classList.add("scroll");
+
+        }else{
+            skillText.classList.remove("scroll");
+        }
+
+
+
+}
+
+
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyMenu() {
+
+    console.log(sticky);
 
     if (window.scrollY >= sticky) {
         navbar.classList.add("sticky")
@@ -19,6 +44,8 @@ function myFunction() {
         
     }
 };
+
+
 
 
 //window.addEventListener('scroll', myFunction);
